f24d47fcd13dadbe0ebd185a11ad3b866e6075ef
Mahmut Gülen <mahmutandroit@gmail.com>
2023-07-25 18:23:33 +0300

    sdf

--- CvProject.UI/Controllers/CvController.cs
+++ CvProject.UI/Controllers/CvController.cs
@@ -3,15 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.UI;
 
 namespace CvProject.UI.Controllers
 {
-    public class CvController : Controller
-    {
-        // GET: Cv
-        public ActionResult Index()
-        {
-            return View();
-        }
-    }
+	public class CvController : Controller
+	{
+		// GET: Cv
+		private readonly IUserService _userService;
+
+		public ActionResult Index()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public ActionResult GetUser()
+		{
+			var result = _userService.GetUser(1);
+			return View(result);
+		}
+	}
 }
