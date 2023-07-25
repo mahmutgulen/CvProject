1109e5f82e9726521ca70446a96a8444a3bbd0f9
Mahmut Gülen <mahmutandroit@gmail.com>
2023-07-26 02:09:43 +0300

    dfg

--- CvProject.UI2/Controllers/CvController.cs
+++ CvProject.UI2/Controllers/CvController.cs
@@ -12,11 +12,23 @@ namespace CvProject.UI2.Controllers
             _userService = userService;
         }
 
-        [HttpGet]
         public IActionResult Index()
         {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult GetUser()
+        {
             var result = _userService.GetUser(1).Data;
+            ViewBag.UserName = result.UserName;
+            ViewBag.UserSurname = result.UserSurname;
+            ViewBag.UserPhoneNumber = result.UserPhoneNumber;
+            ViewBag.UserMail = result.UserMail;
+            ViewBag.Position = "Position Get Shit Done!";
+
             return View(result);
         }
+
     }
 }
