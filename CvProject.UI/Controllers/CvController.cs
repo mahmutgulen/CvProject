c34571fe63ccc7bf6c2026a422e2af315eae0b38
Mahmut Gülen <mahmutandroit@gmail.com>
2023-07-25 18:11:34 +0300

    Bu büyük bi değişikilk

---
+++ CvProject.UI/Controllers/CvController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CvProject.UI.Controllers
+{
+    public class CvController : Controller
+    {
+        // GET: Cv
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ CvProject.UI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CvProject.UI.MvcApplication" Language="C#" %>
