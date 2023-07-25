3c322a93980b8bc95f5e4cbf8392d4c9ea5864ce
Mahmut Gülen <mahmutandroit@gmail.com>
2023-07-26 00:42:33 +0300

    sdf

---
+++ CvProject.UI2/Controllers/CvController.cs
@@ -0,0 +1,21 @@
+﻿using CvProject.BLL.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CvProject.UI2.Controllers
+{
+	public class CvController : Controller
+	{
+		private readonly IUserService _userService;
+
+		public CvController(IUserService userService)
+		{
+			_userService = userService;
+		}
+
+		[HttpGet]
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
---
+++ CvProject.UI2/Program.cs
@@ -0,0 +1,35 @@
+using Autofac.Extensions.DependencyInjection;
+using Autofac;
+using CvProject.BLL.DependencyResolvers.Autofac;
+
+var builder = WebApplication.CreateBuilder(args);
+//autofac
+builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory())
+	.ConfigureContainer<ContainerBuilder>(builder =>
+	{
+		builder.RegisterModule(new AutofacBusinessModule());
+	});
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+	app.UseExceptionHandler("/Error");
+	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+	app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
