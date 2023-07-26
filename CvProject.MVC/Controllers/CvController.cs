aebb632de234b707c0301f57bbbd70c19c9dc49d
Mahmut Gülen <mahmutandroit@gmail.com>
2023-07-26 21:41:08 +0300

    asd

---
+++ CvProject.MVC/Controllers/CvController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CvProject.MVC.Controllers
+{
+    public class CvController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ CvProject.MVC/Program.cs
@@ -0,0 +1,26 @@
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using CvProject.BLL.DependencyResolvers.Autofac;
+
+var builder = WebApplication.CreateBuilder(args);
+
+//autofac
+builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory())
+    .ConfigureContainer<ContainerBuilder>(builder =>
+    {
+        builder.RegisterModule(new AutofacBusinessModule());
+    });
+
+builder.Services.AddMvc();
+
+
+var app = builder.Build();
+
+app.UseStaticFiles();
+
+app.MapControllerRoute(
+    name: "Default",
+    pattern: "{controller=Cv}/{action=Index}");
+
+
+app.Run();
