a7479188760ff5053575fcde06ad3ff97bc27723
Mahmut Gülen <mahmutandroit@gmail.com>
2023-07-27 02:35:38 +0300

    skill component add

---
+++ CvProject.MVC/ViewComponents/GetSkill.cs
@@ -0,0 +1,21 @@
+﻿using CvProject.BLL.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CvProject.MVC.ViewComponents
+{
+    public class GetSkill : ViewComponent
+    {
+        private readonly IUserSkillService _userSkillService;
+
+        public GetSkill(IUserSkillService userSkillService)
+        {
+            _userSkillService = userSkillService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var result = _userSkillService.GetUserSkill(1).Data;
+            return View(result);
+        }
+    }
+}
