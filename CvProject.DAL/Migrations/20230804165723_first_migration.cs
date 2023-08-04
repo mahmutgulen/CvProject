a271689e14e7081402cd58faf668c3fb9753106c
Mahmut Gülen <mahmutandroit@gmail.com>
2023-08-04 19:57:55 +0300

    sdf

---
+++ CvProject.DAL/Migrations/20230804165723_first_migration.cs
@@ -0,0 +1,247 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CvProject.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class first_migration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UserAddresses",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<int>(type: "int", nullable: false),
+                    UserCountry = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UserCity = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UserDistrict = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UserAddressStatus = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserAddresses", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserCertificates",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<int>(type: "int", nullable: false),
+                    CertificateDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CertificateProvider = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CertificateName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CertificateCountry = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CertificateCity = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CertificateStatus = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserCertificates", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserDescriptions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<int>(type: "int", nullable: false),
+                    UserDescriptionText = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UserStatus = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserDescriptions", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserEducations",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<int>(type: "int", nullable: false),
+                    EducationName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    EducationBranch = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    EducationCountry = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    EducationCity = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    EducationStartDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    EducationEndDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    EducationStatus = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserEducations", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserExperiences",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<int>(type: "int", nullable: false),
+                    CompanyName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CompanyCountry = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CompanyCity = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Position = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ExperienceStartDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ExperienceEndDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UserExperienceStatus = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserExperiences", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserInterests",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<int>(type: "int", nullable: false),
+                    InterestName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    InterestStatus = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserInterests", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserLanguages",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<int>(type: "int", nullable: false),
+                    LanguageName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LanguageLevel = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LanguageStatus = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserLanguages", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserReferences",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<int>(type: "int", nullable: false),
+                    ReferenceCompany = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ReferenceName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ReferenceSurname = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ReferencePhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ReferenceMail = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ReferenceStatus = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserReferences", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UserFirstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UserSurname = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UserMail = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UserPhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UserImage = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    UserPassword = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UserStatus = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserSkills",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<int>(type: "int", nullable: false),
+                    SkillName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    SkillProgress = table.Column<byte>(type: "tinyint", nullable: false),
+                    SkillStatus = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserSkills", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserSocialMedias",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<int>(type: "int", nullable: false),
+                    SocialMediaName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    SocialMediaIcon = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    SocialMediaLink = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    SocialMediaStatus = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserSocialMedias", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserAddresses");
+
+            migrationBuilder.DropTable(
+                name: "UserCertificates");
+
+            migrationBuilder.DropTable(
+                name: "UserDescriptions");
+
+            migrationBuilder.DropTable(
+                name: "UserEducations");
+
+            migrationBuilder.DropTable(
+                name: "UserExperiences");
+
+            migrationBuilder.DropTable(
+                name: "UserInterests");
+
+            migrationBuilder.DropTable(
+                name: "UserLanguages");
+
+            migrationBuilder.DropTable(
+                name: "UserReferences");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "UserSkills");
+
+            migrationBuilder.DropTable(
+                name: "UserSocialMedias");
+        }
+    }
+}
