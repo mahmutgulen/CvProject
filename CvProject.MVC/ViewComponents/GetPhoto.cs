41e7223999b134a752ba68af50981192123d1cf5
Mahmut Gülen <mahmutandroit@gmail.com>
2023-08-05 02:37:00 +0300

    asdsa

---
+++ CvProject.MVC/ViewComponents/GetPhoto.cs
@@ -0,0 +1,24 @@
+﻿using CvProject.CORE.Utilities.Result;
+using CvProject.DAL.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CvProject.MVC.ViewComponents
+{
+    public class GetPhoto : ViewComponent
+    {
+        private readonly IUserDal _userDal;
+
+        public GetPhoto(IUserDal userDal)
+        {
+            _userDal = userDal;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var userId = Convert.ToInt32(HttpContext.User.Claims.FirstOrDefault(x => x.Type == System.Security.Claims.ClaimTypes.NameIdentifier).Value);
+            var image = _userDal.Get(x => x.Id == userId).UserImage;
+            ViewBag.Image = image;
+            return View();
+        }
+    }
+}
